65d2bd24a8c3de6740ebc8929205f1805b621bc7
ezecap01 <ezequielanaya2007@gmail.com>
2025-11-06 15:29:49 -0300

    Add files via upload

---
+++ parte.js
@@ -0,0 +1,24 @@
+const precios = {
+    "Zapatillas": 125.000,
+    "Remera": 70.000,
+    "Pantalón": 90.000
+};
+
+function actualizarPrecio() {
+    const producto = document.getElementById("producto").value;
+    const precioInput = document.getElementById("precio");
+
+    if (producto && precios[producto]) {
+        precioInput.value = "$" + precios[producto].toLocaleString();
+    } else {
+        precioInput.value = "";
+    }
+}
+
+document.getElementById("mostrarLista").addEventListener("click", () => {
+    document.getElementById("tablaPrecios").classList.remove("oculto");
+});
+
+document.getElementById("cerrarLista").addEventListener("click", () => {
+    document.getElementById("tablaPrecios").classList.add("oculto");
+});
